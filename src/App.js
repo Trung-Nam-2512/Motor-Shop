71685a320038d098de1e6bcfc5112f33bdf96ada
Trung-Nam-2512 <139524140+Trung-Nam-2512@users.noreply.github.com>
2024-04-11 10:39:42 +0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
 
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import Header from './components/header';
+import BannerSlider from './components/banner';
+import IntroduceHome from './features/introduce_home'
+import ProductList from './features/product';
+import ServiceHome from './features/service_home';
+import News from './features/news';
+import NewsPaper from './features/newspaper';
+import FooterImg from './features/footerlogo';
+import Footer from './features/footer';
+import ProductPages from './components/productPages';
+import HomePages from './components/home';
+import Intro from './components/intro';
+import NewsPages from './components/news';
+import ServicePages from './components/servicee';
+import LoginForm from './components/loginfrom';
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+   
+   <Router>
+      <Routes>
+        <Route path="/" element={<HomePages />} />
+        <Route path="/sanpham"  element={<ProductPages />} />
+        <Route path="/sanpham/Kawasaki"  element={<ProductPages />} />
+        <Route path="/sanpham/honda"  element={<ProductPages />} />
+        <Route path="/thongtincanhan"  element={<Intro />} />
+        <Route path="/tintuc"  element={<NewsPages />} />
+        <Route path="/dichvu"  element={<ServicePages />} />
+        <Route path="/lienhe"  element={<LoginForm />} />
+        
+      </Routes>
+    </Router>
+      </div>
   );
 }
 
